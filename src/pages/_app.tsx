73c255f170e58b7cccea8f1e2ef9a9666b5febf0
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-06-24 19:38:11 -0300

    styles: add Header styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import { Container, Header } from "../styles/pages/app"
+import logoImg from "../assets/logo.svg"
 
+/* 
+* App é carregado em todas as páginas. É algo global, por isso o
+* Header está aqui. Se fosse algo específico de uma página, deveria
+* ficar na página em si.
+*/
+
+// globalStyles deve ficar fora para que não seja recriado a cada render
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  globalStyles()
-  
-  return <Component {...pageProps} />;
+
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
