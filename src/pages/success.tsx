04f4dfa2da4335d484e1da85018239646683624c
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-07-01 20:52:50 -0300

    feat: add  success styles

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,7 +1,22 @@
-export function Success() {
+import { SuccessContainer, ImageContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
+export default function Success() {
   return (
-    <div>
-      <span>Success </span>
-    </div>
+    <SuccessContainer>
+      <h1>Compra efetuada</h1>
+
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <p>
+        Uhuul <strong>Stefany Sá</strong>, sua <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">
+        Voltar ao catálogo
+      </Link>
+    </SuccessContainer>
   )
 }
